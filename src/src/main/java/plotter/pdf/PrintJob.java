5935b14ce4c8de335e3d73597f94affeb59e03f7
Samuel Vogel <samuel.vogel@viison.com>
2011-10-25 13:03:19 +0200

    Use alpha channel in ghostscript

--- src/src/main/java/plotter/pdf/PrintJob.java
+++ src/src/main/java/plotter/pdf/PrintJob.java
@@ -94,6 +94,8 @@ public class PrintJob extends File {
 		command.add("-dSAFER");
 		command.add("-r" + resolution);
 		command.add("-sDEVICE=png16m");
+		command.add("dTextAlphaBits=4");
+		command.add("-dGraphicsAlphaBits=4");
 		command.add("-sOutputFile=" + tmp.getAbsolutePath());
 		command.add(this.getAbsolutePath());
 
